b453308c46d2ff382e4da2d8700742b181564087
Bastien2708 <bastien.bussard04@hotmail.com>
2022-08-29 12:54:28 +0200

    Version fonctionnelle

--- Ex02 - Lecture_JDBC_Eleve/src/app/workers/DbWorker.java
+++ Ex02 - Lecture_JDBC_Eleve/src/app/workers/DbWorker.java
@@ -23,7 +23,7 @@ public class DbWorker implements DbWorkerItf {
     @Override
     public void connecterBdMySQL(String nomDB) throws MyDBException {
         final String url_local = "jdbc:mysql://localhost:3306/" + nomDB;
-        final String url_remote = "jdbc:mysql://LAPEMFB37-21.edu.net.fr.ch:3306/" + nomDB;
+        final String url_remote = "jdbc:mysql://localhost:3306/" + nomDB;
         final String user = "root";
         final String password = "emf123";
 
@@ -72,21 +72,44 @@ public class DbWorker implements DbWorkerItf {
 
     public List<Personne> lirePersonnes() throws MyDBException {
         listePersonnes = new ArrayList<>();
-        
+        try {
+            Statement st = dbConnexion.createStatement();
+            ResultSet rs = st.executeQuery("select PK_PERS, Prenom, Nom from t_personne");
+            while (rs.next()) {
+                String nom = rs.getString("NOM");
+                String prenom = rs.getString(2);
+                int pk = rs.getInt("PK_PERS");
+                listePersonnes.add(new Personne(nom, prenom));
+            }
+        } catch (SQLException ex) {
+        }
         return listePersonnes;
     }
 
     @Override
     public Personne precedentPersonne() throws MyDBException {
-
-        return null;
-
+        lirePersonnes();
+        Personne a = new Personne();
+        if (index == 0) {
+            a = listePersonnes.get(0);
+        } else {
+            a = listePersonnes.get(index - 1);
+            index--;
+        }
+        return a;
     }
 
     @Override
     public Personne suivantPersonne() throws MyDBException {
-
-        return null;
+        lirePersonnes();
+        Personne a = new Personne();
+        if (index == listePersonnes.size()-1) {
+            a = listePersonnes.get(listePersonnes.size());
+        } else {
+            a = listePersonnes.get(index + 1);
+            index++;
+        }
+        return a;
 
     }
 
